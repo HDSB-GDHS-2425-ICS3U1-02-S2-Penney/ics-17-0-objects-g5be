e6453039f7e7d538932650c355b9005cdf9ebb3d
g5be <1bukgab@hdsb.ca>
2025-05-26 13:17:36 -0400

    art

--- script.js
+++ script.js
@@ -3,37 +3,51 @@
 // Call the init function when the HTML window loads
 window.onload = init;
 
-function init() {
-  // Init function that sets up our canvas. 
-  canvas = document.getElementById('myCanvas');
-  ctx = canvas.getContext('2d');
-  // Start the first frame request to begin the game loop
-  window.requestAnimationFrame(gameLoop);
-
-}
+let canvas, ctx;
 
 /**** OBJECT CREATION FUNCTIONS ****/
 
-// Write the circle object function here
+// Circle object function
+function Circle(x, y, radius, color) {
+  this.x = x;
+  this.y = y;
+  this.radius = radius;
+  this.color = color;
 
+  this.draw = function(ctx) {
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+    ctx.fillStyle = this.color;
+    ctx.fill();
+  }
+}
 
+// Create and draw a new circle with random attributes
+function createCircle() {
+  const x = randomInteger(0, canvas.width);
+  const y = randomInteger(0, canvas.height);
+  const radius = randomInteger(10, 50);
+  const color = `rgb(${randomInteger(0,255)}, ${randomInteger(0,255)}, ${randomInteger(0,255)})`;
+  
+  const circle = new Circle(x, y, radius, color);
+  circle.draw(ctx);
+}
 
-// Write the createCircle function here. 
+// Returns a random integer between min and max (inclusive)
+function randomInteger(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
+/**** INIT & GAME LOOP ****/
 
-
-// Write the randomInteger function here. 
-
-
-
-/**** GAMELOOP ****/
+function init() {
+  canvas = document.getElementById('myCanvas');
+  ctx = canvas.getContext('2d');
+  window.requestAnimationFrame(gameLoop);
+}
 
 function gameLoop(timestamp) {
-
-  // Call the createCircle function here.
-
-
-  // This causes the game to loop every frame (Do not change this)
+  createCircle(); // Draw a new circle every frame
   window.requestAnimationFrame(gameLoop);
 }
 
